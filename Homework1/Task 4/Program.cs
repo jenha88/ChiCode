ace7661a131812c28f11c1122fbe0e839623262a
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2022-01-30 15:26:07 -0600

    i hate it here

---
+++ Homework1/Task 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           public double LineValueForY()
+            {
+
+            }
+        }
+    }
+}
---
+++ Homework1/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int grade = 0;
+            string answer;
+            do
+            {
+                Console.WriteLine("Please type in your exam score");
+                grade = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Do you have another score to input?: yes/no");
+                answer = Console.ReadLine().ToLower();
+            } while (answer!="yes");
+            if (answer=="no")
+            {
+                
+            }
+        }
+    }
+}
