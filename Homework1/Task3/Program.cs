5352ae18375adf4546fd0b6e0d65a8efef40c540
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2022-01-30 17:50:12 -0600

    testing things

--- Homework1/Task3/Program.cs
+++ Homework1/Task3/Program.cs
@@ -9,17 +9,20 @@ namespace Task3
         {
             
             int grade = 0;
+            int inputs = 0;
             string answer;
             do
             {
                 Console.WriteLine("Please type in your exam score");
                 grade = Convert.ToInt32(Console.ReadLine());
+                inputs++;
+
                 Console.WriteLine("Do you have another score to input?: yes/no");
                 answer = Console.ReadLine().ToLower();
             } while (answer!="yes");
             if (answer=="no")
             {
-                
+                Console.WriteLine($"Your total grade entry is{inputs}");
             }
         }
     }
