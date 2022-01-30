9d72167803c7811afb3466247c2ebc7c1490f81d
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2022-01-30 15:50:48 -0600

    i hate it here part2

--- Homework1/Task 4/Program.cs
+++ Homework1/Task 4/Program.cs
@@ -4,12 +4,36 @@ namespace Task_4
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-           public double LineValueForY()
-            {
+            Console.WriteLine(LineValueForY());
+            Factorial();
+          
+            
+        }
+ 
+        public static double LineValueForY(double m, double x, double b)
+        {
+            Console.WriteLine("Please type in the slope (m)");
+            m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please type in the x-value:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please type in the y-value:");
+            b = Convert.ToDouble(Console.ReadLine());
 
+            double answer = m * x + b;
+
+            return answer;
+        }
+        public  double Factoiral(int num)
+        {
+            double result = 1;
+            while (num != 1)
+            {
+                result = result * num;
+                num = num - 1;
             }
+            return result;
         }
     }
 }
