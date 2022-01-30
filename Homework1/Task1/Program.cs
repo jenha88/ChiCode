496a156ae0fe35b103e80926c97f52a134a63242
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2022-01-30 01:02:35 -0600

    attempting things

---
+++ Homework1/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double avg = 0;
+            double grade = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter in your exam grade:");
+                grade = Convert.ToDouble(Console.ReadLine());
+                avg = grade / 3;
+            }
+   
+            Console.WriteLine($"The average for the exams: {avg}");
+
+
+
+
+
+
+        }
+    }
+}
